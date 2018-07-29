c78c37548136bdc8b7ea2ef3cf54dde24708cf89
Simon Cass <scass1991@gmail.com>
2018-07-29 19:18:30 +0100

    initial commit

---
+++ helloworld.rb
@@ -0,0 +1,6 @@
+#July 29th 2018
+#The print command just takes whatever you give it and prints it to the screen.
+#puts (for "put string") is slightly different: it adds a new (blank)
+#line after the thing you want it to print - no need for brackets
+puts "Hello world!"
+print "Hello world!"
