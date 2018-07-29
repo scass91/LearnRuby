f2e1d9cb338bb71a5a2563dde001e445d77d5eaa
Simon Cass <scass1991@gmail.com>
2018-07-29 19:24:14 +0100

    initial commit

---
+++ methods.rb
@@ -0,0 +1,13 @@
+#July 29th 2018
+#Various methods for string manipulation
+
+puts "Simon Cass".length
+puts "Simon Cass".reverse
+puts "Simon Cass".downcase
+puts "Simon Cass".upcase
+
+=begin
+multi-line comments
+can be added without #
+by using =begin & =end
+=end
