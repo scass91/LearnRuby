49b97575bf00ef4256d49f3d41852bc48ae160ee
Simon Cass <scass1991@gmail.com>
2018-07-29 19:27:08 +0100

    initial commit

---
+++ maths.rb
@@ -0,0 +1,12 @@
+#July 29th 2018
+#Some mathematical functions...
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+indices = 300 ** 3
+
+puts sum
+puts product
+puts quotient
+puts indices
