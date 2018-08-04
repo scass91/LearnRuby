c6832a0625586de2cddbf4814fd39d5bd2dd821b
Simon Cass <scass1991@gmail.com>
2018-08-04 14:30:47 +0100

    initial commit

---
+++ virtualmachine.rb
@@ -0,0 +1,26 @@
+=begin
+August 4th 2018
+Virtual computer in ruby
+=end
+
+class Computer
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created at #{time} by #{@username}. "
+  end
+
+  def Computer.get_users
+    @@users
+  end
+end
+
+my_computer = Computer.new("dave", "davidson")
